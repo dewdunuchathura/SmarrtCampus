42d7247af5367bf64bae9629bd5825098ba98d50
KrishanDilshara <dilsharakrishan@gmail.com>
2026-04-19 19:00:44 +0530

    Add notification service with basic logic

---
+++ backend/src/main/java/com/fms/notifications/NotificationService.java
@@ -0,0 +1,21 @@
+package com.fms.notifications;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NotificationService {
+
+    @Autowired
+    private NotificationRepository notificationRepository;
+
+    public Notification createNotification(Notification notification) {
+        return notificationRepository.save(notification);
+    }
+
+    public List<Notification> getUserNotifications(String userId) {
+        return notificationRepository.findByUserId(userId);
+    }
+}
