e5ca5be59864ce47ac6e76ad0b135899d0bd3d3b
KrishanDilshara <dilsharakrishan@gmail.com>
2026-04-19 18:59:25 +0530

    Add Notification repository

---
+++ backend/src/main/java/com/fms/notifications/NotificationRepository.java
@@ -0,0 +1,10 @@
+package com.fms.notifications;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
+
+public interface NotificationRepository extends MongoRepository<Notification, String> {
+
+    List<Notification> findByUserId(String userId);
+
+}
