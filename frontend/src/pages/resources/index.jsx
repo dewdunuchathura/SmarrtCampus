da682de4c6ea933d420d56c432dd2cff5185d07e
dewdunuchathura <dewdunuc1990@gmail.com>
2026-04-21 18:17:21 +0530

    update resources page

--- frontend/src/pages/resources/index.jsx
+++ frontend/src/pages/resources/index.jsx
@@ -1,3 +1,13 @@
 export default function ResourcesPage() {
-  return <h1>Resources Module</h1>;
+  return (
+    <main style={{ padding: '48px 20px' }}>
+      <div style={{ maxWidth: '720px', margin: '0 auto' }}>
+        <h1>Resources Module</h1>
+        <p>
+          This page will be used to show rooms, labs, and equipment once the resources workflow is
+          connected.
+        </p>
+      </div>
+    </main>
+  );
 }
